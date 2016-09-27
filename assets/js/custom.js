c0175b17bf25642b56e7735790cb31f923602ad1
Alexander Lundén <alexander.lunden@telia.com>
2016-09-27 13:29:34 +0200

    Completed sections Categories, Users and Misc. JS changes.
    Stripped js libraries from theDocs.js and included them separately (new
    file is theDocs.ntgt.js).
    Added script which emphasizes HTTP verbs and parameters in request
    labels.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,16 @@
-$(function() {
+function emphasizeHTTP(str) {
+    // Emphasize HTTP verbs.
+    str = str.replace(/^(GET|POST|PUT|DELETE)(\s.+)$/, "<b>$1</b>$2");
 
+    // Emphasize URL parameters.
+    //s = s.replace("&lt;", "&lt;<i>").replace("&gt;", "</i>&gt;");
+    str = str.replace(/&lt;([^&]+)&gt;/ig, "&lt;<i>$1</i>&gt;");
 
+    return str;
+}
 
+$(function () {
+    $(".ntg-http-request").each(function () {
+        $(this).html(emphasizeHTTP($(this).html()));
+    });
 })(jQuery);
