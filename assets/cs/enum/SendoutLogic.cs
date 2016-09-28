6e9aa0270f8bcf69e8b5a093816995499350e132
Alexander Lundén <alexander.lunden@telia.com>
2016-09-28 09:58:40 +0200

    enum -> PseudoEnum : string

--- assets/cs/enum/SendoutLogic.cs
+++ assets/cs/enum/SendoutLogic.cs
@@ -1,9 +1,9 @@
-﻿enum SendoutLogic
+﻿PseudoEnum SendoutLogic : string
 {
-    ALL,
-    NOT_SENT,
-    NOT_ACCESSED,
-    NOT_COMPLETED,
-    COMPLETED,
-    VISITED_NOT_COMPLETED
+    "ALL",
+    "NOT_SENT",
+    "NOT_ACCESSED",
+    "NOT_COMPLETED",
+    "COMPLETED",
+    "VISITED_NOT_COMPLETED"
 }
